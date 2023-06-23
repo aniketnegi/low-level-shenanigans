90ecdd1b5d6637c45b85db48900d392a0362f6b2
aniketnegi <aniket.negi19104@gmail.com>
2023-06-23 08:39:30 +0530

    simplified list insertion and deletion, better error handling
    insertion and deletion are now abstracted to another function. Error reporting is done through their own separate helper functions

--- doubly-linked-lists/linked_list.c
+++ doubly-linked-lists/linked_list.c
@@ -13,16 +13,52 @@ typedef struct list {
   struct list_node *head, *tail;
 } list_t;
 
-static node_t *list_insert(node_t *node_prev, node_t *node_next,
+static void assert_valid_list_ptr(const list_t *list) {
+  if (list == NULL) {
+    // ERR -> list use uninitialized
+    fprintf(stderr, "Error: list is uninitialized\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+static void assert_list_not_empty(const list_t *list) {
+  // tests don't specify how to deal with empty list so i just exit
+  if (list->head == NULL) {
+    // ERR -> list is empty
+    fprintf(stderr, "Error: list is empty\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+static node_t *list_insert(list_t *list, node_t *node_prev, node_t *node_next,
                            ll_data_t data) {
-  node_t *new_node = malloc(sizeof(node_t) + sizeof(ll_data_t));
+  node_t *new_node = malloc(sizeof(node_t));
 
   if (new_node == NULL) {
-    /// malloc failure
+    // ERR -> malloc failure
     fprintf(stderr, "ERR: malloc failure\n");
     exit(EXIT_FAILURE);
   }
+
   new_node->data = data;
+
+  if (node_prev == NULL && node_next == NULL) {
+    // list is currently empty
+    list->head = new_node;
+    list->tail = new_node;
+  } else if (node_prev == NULL) {
+    // node is to be inserted at beginning
+    list->head = new_node;
+    node_next->prev = new_node;
+  } else if (node_next == NULL) {
+    // node is to be inserted at end
+    list->tail = new_node;
+    node_prev->next = new_node;
+  } else {
+    // node to be inserted in the middle somewhere
+    node_prev->next = new_node;
+    node_next->prev = new_node;
+  }
 
   new_node->prev = node_prev;
   new_node->next = node_next;
@@ -30,47 +66,30 @@ static node_t *list_insert(node_t *node_prev, node_t *node_next,
   return new_node;
 }
 
-static ll_data_t list_remove(list_t *list, node_t *node, size_t situation) {
-  /// situation is used to determine if node has next or prev pointer = NULL /
-  /// OBVIOUSLY:
-  ///   next = NULL ==> node is at list->tail /
-  ///   prev = NULL ==> node is at list->head /
-  ///   neither is NULL ==> node is somewhere in the middle /
-  /// prev = NULL ==> situation = -1 /
-  /// both = NULL ==> situation = 0 /
-  /// next = NULL ==> situation = 1 /
-  /// case default ==> neither = NULL
+static ll_data_t list_remove(list_t *list, node_t *node) {
   ll_data_t data = node->data;
 
   node_t *prev = node->prev;
   node_t *next = node->next;
 
-  switch (situation) {
-  case -1:
-    // node is at list->head
-    list->head = next;
-    next->prev = NULL;
-    break;
-
-  case 0:
-    // list only has one element
-    // prev = next = NULL
-    // can use them also, but code doesn't read very good IMO
+  if (prev == NULL && next == NULL) {
+    // only 1 element in list
     list->head = NULL;
     list->tail = NULL;
-    break;
-
-  case 1:
-    // node is at list->tail
+  } else if (prev == NULL) {
+    // node is head
+    list->head = next;
+    next->prev = NULL;
+  } else if (next == NULL) {
+    // node is tail
     list->tail = prev;
     prev->next = NULL;
-    break;
-
-  default:
-    // somwhere in the middle
+  } else {
+    // node is in middle
     prev->next = next;
     next->prev = prev;
   }
+
   free(node);
   return data;
 }
@@ -80,7 +99,7 @@ list_t *list_create(void) {
   list_t *new_list = malloc(sizeof(list_t));
 
   if (new_list == NULL) {
-    /// malloc failure
+    // ERR -> malloc failure
     fprintf(stderr, "ERR: malloc failure\n");
     exit(EXIT_FAILURE);
   }
@@ -93,10 +112,9 @@ list_t *list_create(void) {
 
 // counts the items on a list
 size_t list_count(const list_t *list) {
+  assert_valid_list_ptr(list);
+
   size_t list_size = 0;
-  if (list == NULL) {
-    return list_size;
-  }
 
   node_t *curr = list->head;
 
@@ -110,117 +128,59 @@ size_t list_count(const list_t *list) {
 
 // inserts item at back of a list
 void list_push(list_t *list, ll_data_t item_data) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
-  }
+  assert_valid_list_ptr(list);
 
-  if (list_count(list) == 0) {
-    // empty list
-    node_t *node = list_insert(NULL, NULL, item_data);
-
-    list->head = node;
-    list->tail = node;
-
-    return;
-  }
-  // list has one or more elements
-  node_t *node = list_insert(list->tail, NULL, item_data);
-
-  list->tail->next = node;
-  list->tail = node;
+  // (list_t *) to the newly created node returned by list_insert can be used
+  list_insert(list, list->tail, NULL, item_data);
 }
 
 // removes item from back of a list
 ll_data_t list_pop(list_t *list) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
-  }
+  assert_valid_list_ptr(list);
 
   ll_data_t req_data;
 
-  if (list_count(list) == 0) {
-    // empty list
-    // tests don't specify how to handle err
-    fprintf(stderr, "Error: Can't pop from empty list\n");
-    return -1; // or exit(EXIT_FAILURE)
-  } else if (list_count(list) == 1) {
-    req_data = list_remove(list, list->tail, 0);
-  } else {
-    // list has 2 or more elements
-    req_data = list_remove(list, list->tail, 1);
-  }
+  assert_list_not_empty(list);
+
+  // list has 1  or more element
+  req_data = list_remove(list, list->tail);
 
   return req_data;
 }
 
 // inserts item at front of a list
 void list_unshift(list_t *list, ll_data_t item_data) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
-  }
+  assert_valid_list_ptr(list);
 
-  if (list_count(list) == 0) {
-    // empty list
-    node_t *node = list_insert(NULL, NULL, item_data);
-
-    list->head = node;
-    list->tail = node;
-
-    return;
-  }
-  // list has 1 element or more elements
-  node_t *node = list_insert(NULL, list->head, item_data);
-
-  list->head->prev = node;
-  list->head = node;
+  // (list_t *) to the newly created node returned by list_insert can be used
+  list_insert(list, NULL, list->head, item_data);
 }
 
 // removes item from front of a list
 ll_data_t list_shift(list_t *list) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
-  }
+  assert_valid_list_ptr(list);
 
   ll_data_t req_data;
 
-  if (list_count(list) == 0) {
-    // empty list
-    // tests don't specify how to handle err
-    fprintf(stderr, "Error: Can't pop from empty list\n");
-    return -1; // or exit(EXIT_FAILURE)
-  } else if (list_count(list) == 1) {
-    req_data = list_remove(list, list->head, 0);
-  } else {
-    // list has 2 or more elements
-    req_data = list_remove(list, list->head, -1);
-  }
+  assert_list_not_empty(list);
+
+  // list has 1 or more elements
+  req_data = list_remove(list, list->head);
 
   return req_data;
 }
 
 // deletes a node that holds the matching data
 void list_delete(list_t *list, ll_data_t data) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
-  }
+  assert_valid_list_ptr(list);
 
-  if (list_count(list) == 0) {
+  if (list->head == NULL) {
     // empty list
     // DO NOTHING
     return;
-  } else if (list_count(list) == 1) {
+  } else if (list->head == list->tail) {
     if (list->head->data == data) {
-      list_remove(list, list->head, 0);
+      list_remove(list, list->head);
       return;
     } else {
       // data not found
@@ -231,21 +191,7 @@ void list_delete(list_t *list, ll_data_t data) {
     for (node_t *curr = list->head; curr != NULL; curr = curr->next) {
       if (curr->data == data) {
         // delete this node
-        node_t *previous = curr->prev;
-        node_t *following = curr->next;
-        size_t situation;
-        if (previous == NULL) {
-          // element to be removed is first element
-          situation = -1;
-        } else if (following == NULL) {
-          // element to be removed is last element
-          situation = 1;
-        } else {
-          // element to be removes is neither first nor last element
-          situation = 69; // switch-case will go to default
-        }
-
-        list_remove(list, curr, situation);
+        list_remove(list, curr);
         return;
       }
     }
@@ -255,18 +201,11 @@ void list_delete(list_t *list, ll_data_t data) {
 // destroys an entire list
 // list will be a dangling pointer after calling this method on it
 void list_destroy(list_t *list) {
-  if (list == NULL) {
-    // ERR -> list use uninitialized
-    fprintf(stderr, "Error: list is uninitialized\n");
-    exit(EXIT_FAILURE);
+  assert_valid_list_ptr(list);
+
+  while (list->head != NULL) {
+    list_pop(list);
   }
 
-  node_t *curr = list->head;
-
-  while (curr != NULL) {
-    node_t *nextNode = curr->next;
-    free(curr);
-    curr = nextNode;
-  }
-  free(list);
+  list = NULL;
 }
